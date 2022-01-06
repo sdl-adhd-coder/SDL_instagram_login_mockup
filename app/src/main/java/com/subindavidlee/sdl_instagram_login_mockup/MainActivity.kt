2fc2213a1933ccc5f54963e841a301cc353840c1
sdl-adhd-coder <96659361+sdl-adhd-coder@users.noreply.github.com>
2022-01-06 14:53:18 +0900

    Initial commit

---
+++ app/src/main/java/com/subindavidlee/sdl_instagram_login_mockup/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.subindavidlee.sdl_instagram_login_mockup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
